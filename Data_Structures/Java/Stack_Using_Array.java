6c812270a883bce0dd88a413ce172d2781b14ec6
Kamran Khan <silentfinisher786@gmail.com>
2022-10-22 21:50:29 +0530

    Added Algorithms

---
+++ Data_Structures/Java/Stack_Using_Array.java
@@ -0,0 +1,65 @@
+package Data_Structures.Java;
+
+public class Stack_Using_Array {
+  private int arr[];
+  private int top;
+  private int capacity;
+
+  // Creating a stack
+  Stack_Using_Array(int size) {
+    arr = new int[size];
+    capacity = size;
+    top = -1;
+  }
+
+  public void push(int x) {
+    if (isFull()) {
+      System.out.println("OverFlow\nProgram Terminated\n");
+      System.exit(1);
+    }
+
+    System.out.println("Inserting " + x);
+    arr[++top] = x;
+  }
+
+  public int pop() {
+    if (isEmpty()) {
+      System.out.println("STACK EMPTY");
+      System.exit(1);
+    }
+    return arr[top--];
+  }
+
+  public int size() {
+    return top + 1;
+  }
+
+  public Boolean isEmpty() {
+    return top == -1;
+  }
+
+  public Boolean isFull() {
+    return top == capacity - 1;
+  }
+
+  public void printStack() {
+    for (int i = 0; i <= top; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+
+  public static void main(String[] args) {
+    Stack_Using_Array stack = new Stack_Using_Array(5);
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+
+    stack.pop();
+    System.out.println("\nAfter popping out");
+
+    stack.printStack();
+
+  }
+}
